fb7891011354b849b2b7fc1dba3fc9e5627e2dca
Hang Cui <oscarcuihang@hotmail.com>
2019-02-13 11:08:50 +0800

    SL-002 feat(DB) Add mongo db (#2)

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'rails'
+# Pick the frameworks you want:
+#require "active_model/railtie"
+require "active_job/railtie"
+#require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,8 +29,11 @@ module Blank
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.orm :mongoid
+    end
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
