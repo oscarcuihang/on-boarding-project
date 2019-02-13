8284faf97d2c7a8e66157b39f86d90eb778d1674
Hang Cui <oscarcuihang@hotmail.com>
2019-02-13 12:03:06 +0800

    SL-004 feat(User) Add user name api (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users do
     get 'count', on: :collection
+    member do
+      get 'name'
+    end
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
