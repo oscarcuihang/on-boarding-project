43d2bc7ec371f6060ee146eef427c37d5d75d0a6
Hang Cui <oscarcuihang@hotmail.com>
2019-02-13 08:44:00 +0000

    SL-005 feat(User) Add new validation and new fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User
   include Mongoid::Document
+
   field :first_name, type: String
-  field :last_name, type: String 
+  field :last_name, type: String
+  field :age, type: Integer
+  field :gender, type: Integer
+
+  validates_presence_of :first_name, :last_name
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :gender, inclusion: { in: [ 0, 1, 2 ] }
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :first_name, :last_name
+json.extract! user, :id, :first_name, :last_name, :age, :gender
 json.url user_url(user, format: :json)
