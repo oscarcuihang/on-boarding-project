cd76e96aeb8535e1b06a70df1eab60644fd2a3ef
Hang Cui <oscarcuihang@hotmail.com>
2019-02-18 17:44:24 +0800

    ad

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'mongoid-rspec'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -42,4 +43,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Mongoid::Matchers, type: :model
 end
